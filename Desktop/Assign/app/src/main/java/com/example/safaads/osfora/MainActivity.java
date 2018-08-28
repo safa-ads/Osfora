cf06ec86ca2ec28a08cb3cdf4a0bfc2ce1f69735
narihane <narihanellaithy@gmail.com>
2018-08-28 19:36:54 +0200

    UI
    added most of the features to UI

--- Desktop/Assign/app/src/main/java/com/example/safaads/osfora/MainActivity.java
+++ Desktop/Assign/app/src/main/java/com/example/safaads/osfora/MainActivity.java
@@ -1,13 +1,61 @@
 package com.example.safaads.osfora;
 
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Path;
+import android.graphics.Rect;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.account_layout);
+        int targetWidth = 128;
+        int targetHeight = 128;
+        Bitmap targetBitmap = Bitmap.createBitmap(targetWidth,
+                targetHeight,Bitmap.Config.ARGB_8888);
+
+        RoundedBitmapDrawable roundDrawable = RoundedBitmapDrawableFactory.create(getResources(), targetBitmap);
+        roundDrawable.setCircular(true);
+
+        ImageView imageView = (ImageView)findViewById(R.id.profile_photo);
+        imageView.setImageDrawable(roundDrawable);
+
+        getRoundedShape(imageView);
+    }
+
+
+    /*
+     * Making image in circular shape
+     */
+    public Bitmap getRoundedShape(Bitmap scaleBitmapImage) {
+        // TODO Auto-generated method stub
+        int targetWidth = 128;
+        int targetHeight = 128;
+        Bitmap targetBitmap = Bitmap.createBitmap(targetWidth,
+                targetHeight,Bitmap.Config.ARGB_8888);
+
+        Canvas canvas = new Canvas(targetBitmap);
+        Path path = new Path();
+        path.addCircle(((float) targetWidth - 1) / 2,
+                ((float) targetHeight - 1) / 2,
+                (Math.min(((float) targetWidth),
+                        ((float) targetHeight)) / 2),
+                Path.Direction.CCW);
+
+        canvas.clipPath(path);
+        Bitmap sourceBitmap = scaleBitmapImage;
+        canvas.drawBitmap(sourceBitmap,
+                new Rect(0, 0, sourceBitmap.getWidth(),
+                        sourceBitmap.getHeight()),
+                new Rect(0, 0, targetWidth,
+                        targetHeight), null);
+        return targetBitmap;
     }
 }
